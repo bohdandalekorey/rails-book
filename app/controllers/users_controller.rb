01b83d1cbd4a9370017101c1cf88b1e20d3d4880
bohdandalekorey <bohdandalekorey@gmail.com>
2019-05-06 20:28:25 +0300

    Remove single-quotes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to my first app on Rails!"
+      flash[:success] = 'Welcome to my first app on Rails!'
       redirect_to @user
     else
       render 'new'
