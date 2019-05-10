4eac1ce27c48632a02aa0adc82002a617335a8c7
bohdandalekorey <bohdandalekorey@gmail.com>
2019-05-10 03:34:24 +0300

    Add elements login path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = 'Welcome to my first app on Rails!'
       redirect_to @user
     else
