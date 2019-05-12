c193941abc28fd40f519f5a7abdc67addeaa80ef
bohdandalekorey <bohdandalekorey@gmail.com>
2019-05-12 13:53:40 +0300

    Add empty line

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,8 @@ class User < ApplicationRecord
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
-    if remember_digest.nil?
-      return false
+    return false if remember_digest.nil?
+
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
