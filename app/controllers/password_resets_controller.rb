c487c468c6b33e74106e179e3c477e431f740c3a
bohdandalekorey <bohdandalekorey@gmail.com>
2019-05-29 16:20:26 +0300

    Edit markup

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,11 +3,11 @@ class PasswordResetsController < ApplicationController
   before_action :valid_user, only: [:edit, :update]
   before_action :check_expiration, only: [:edit, :update]
 
-  def new
-  end
+  def new; end
 
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
+
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
@@ -19,19 +19,18 @@ class PasswordResetsController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
-    if params[:user][:password].empty?                  # Case (3)
+    if params[:user][:password].empty?
       @user.errors.add(:password, "can't be empty")
       render 'edit'
-    elsif @user.update_attributes(user_params)          # Case (4)
+    elsif @user.update_attributes(user_params)
       log_in @user
       flash[:success] = 'Password has been reset.'
       redirect_to @user
     else
-      render 'edit'                                     # Case (2)
+      render 'edit'
     end
   end
 
