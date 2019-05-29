953b9c98d385118f762fb87fe68d437480274407
bohdandalekorey <bohdandalekorey@gmail.com>
2019-05-29 18:52:42 +0300

    Edit activation account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      @user.send_activation_email
+      update_attribute(:activated, true)
+      update_attribute(:activated_at, Time.zone.now)
+      # @user.send_activation_email
       flash[:info] = 'Please check your email to activate your account.'
       redirect_to root_url
     else
